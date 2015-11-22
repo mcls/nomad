e08e2e0d76918e3828de5c3a2e4c27019729b681
Maarten Claes <maartencls@gmail.com>
2015-11-22 20:04:26 +0100

    Can generate migration commands for use with cobra lib

---
+++ commands.go
@@ -0,0 +1,54 @@
+package nomad
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func NewMigrationCmd(list *List, migrationDirectory string) *cobra.Command {
+	cmdRoot := &cobra.Command{
+		Use:   "migration",
+		Short: "migration subcommands",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Usage()
+		},
+	}
+
+	cmdNew := &cobra.Command{
+		Use:   "new",
+		Short: "create migration",
+		Run: func(cmd *cobra.Command, args []string) {
+			m := NewMigrator(migrationDirectory)
+			if err := m.Create(args[0]); err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+
+	cmdRun := &cobra.Command{
+		Use:   "run",
+		Short: "run all pending migrations",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := list.Run(); err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+
+	cmdRollback := &cobra.Command{
+		Use:   "rollback",
+		Short: "rollback the most recent migration",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := list.Rollback(); err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+
+	cmdRoot.AddCommand(cmdNew)
+	cmdRoot.AddCommand(cmdRun)
+	cmdRoot.AddCommand(cmdRollback)
+
+	return cmdRoot
+}
